9a1c57020b6fd1c3c80ac63f049701ee04549eb7
ehuntober <ehuntercrypto@gmail.com>
2023-08-22 08:13:59 +0000

    created the model for the application

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 let mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
+mongoose.connect('mongodb://localhost:27017/chatapp');
 
 const app = require('express')();
 
 const http = require('http').Server(app);
+const userRoute = require('./routes/userRoute')
+
+app.use('/', userRoute);
 
 http.listen(3000, ()=>{
     console.log(`Server is running`)
