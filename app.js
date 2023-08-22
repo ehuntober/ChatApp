ef267d7ac197a03fd24eebef2bdf355fd8544400
ehuntober <ehuntercrypto@gmail.com>
2023-08-22 07:33:24 +0000

    application file added

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ let mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
 
-const app = require('express');
+const app = require('express')();
+
+const http = require('http').Server(app);
+
+http.listen(3000, ()=>{
+    console.log(`Server is running`)
+});
