c3363e0a2b299b1836862112130cc0428c09446e
ehuntober <ehuntercrypto@gmail.com>
2023-08-22 13:44:27 +0000

    view for registered added

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+
+
+const registerLoad = async(req,res) =>{
+
+    try{
+
+        res.render('register')
+
+    }
+    catch(error){
+        console.log(error.message)
+    }
+
+
+
+}
+
+const register = async(req,res)=>{
+
+}
+
+module.exports ={
+    registerLoad,
+    register
+}
